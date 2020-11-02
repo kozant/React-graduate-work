6a3bdec28ca09a258e3929d9750a3cdc9cb3f5c1
kozant <diamonddiver99@gmail.com>
2020-11-02 19:28:04 +0300

    Change problems with article and profile components

--- src/components/article-component/article-page/article-page.js
+++ src/components/article-component/article-page/article-page.js
@@ -1,180 +1,172 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { Redirect } from "react-router";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEdit } from "@fortawesome/free-solid-svg-icons";
-import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
-import { getArticle, deleteArticle } from "../../../services/article-service";
-import { getProfile } from "../../../services/profile-service";
-import ErrorComponent from "../../shared/error-component";
-import FollowProfile from "../../profile-component/follow-profile";
-import FavouriteLike from "../favourite-like";
-import withUser from "../../../hocs";
-import Spinner from "../../shared/spinner";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { getArticle, deleteArticle } from '../../../services/article-service';
+import { getProfile } from '../../../services/profile-service';
+import FollowProfile from '../../profile-component/follow-profile';
+import FavouriteLike from '../favourite-like';
+import withUser from '../../../hocs';
+import Spinner from '../../shared/spinner';
 
-import "./article-page.css";
+import './article-page.css';
 
 class ArticlePage extends Component {
-  _isMounted = false;
-  state = {
-    article: {},
-    articleDeleted: false,
-    errorPage: false,
-    loadingPage: true,
-    profile: {},
-  };
+	_isMounted = false;
+	state = {
+		article: {},
+		articleDeleted: false,
+		errorPage: false,
+		loadingPage: true,
+		profile: {},
+	};
 
-  componentDidMount() {
-    this._isMounted = true;
-    this.loadArticle();
-  }
+	componentDidMount() {
+		this._isMounted = true;
+		this.loadArticle();
+	}
 
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
+	componentWillUnmount() {
+		this._isMounted = false;
+	}
 
-  componentDidUpdate(prevProps) {
-    if (this.props.match.params.slug !== prevProps.match.params.slug) {
-      this.loadArticle();
+	componentDidUpdate(prevProps) {
+		if (this.props.match.params.slug !== prevProps.match.params.slug) {
+			this.loadArticle();
+		}
+	}
+
+	loadArticle = () => {
+		const { slug } = this.props.match.params;
+		const { token } = this.props.authInfo;
+		if (!slug) {
+			return;
+		}
+		getArticle(slug)
+			.then((article) => {
+				getProfile(article.author, token)
+					.then((profile) => {
+						if (this._isMounted) {
+							this.setState({
+								profile: profile,
+								article: article,
+								loadingPage: false,
+							});
+						}
+					})
+					.catch((e) => {
+						if (this._isMounted) {
+							this.setState({ errorPage: true });
+						}
+					});
+			})
+			.catch((e) => {
+				if (this._isMounted) {
+					this.setState({ errorPage: true });
+				}
+			});
+	};
+
+	deleteArticle = (slug, token) => {
+		deleteArticle(slug, token).then(() => {
+			this.setState({ articleDeleted: true });
+		});
+	};
+
+	render() {
+		const { article, loadingPage, errorPage, profile, articleDeleted } = this.state;
+
+		const { slug } = this.props.match.params;
+		const { token, username } = this.props.authInfo;
+
+		if (articleDeleted) {
+			return <Redirect to={`/profile/${username}`} />;
     }
-  }
-
-  loadArticle = () => {
-    const { slug } = this.props.match.params;
-    const { token } = this.props.authInfo;
-    if (!slug) {
-      return;
-    }
-    getArticle(slug)
-      .then((article) => {
-        getProfile(article.author, token).then((profile) => {
-          if (this._isMounted) {
-            this.setState({
-              profile: profile,
-              article: article,
-              loadingPage: false,
-            });
-          }
-        });
-      })
-      .catch((e) => {
-        if (this._isMounted) {
-          this.setState({ errorPage: true });
-        }
-      });
-  };
-
-  deleteArticle = (slug, token) => {
-    deleteArticle(slug, token).then(() => {
-      this.setState({ articleDeleted: true });
-    });
-  };
-
-  render() {
-    const {
-      article,
-      loadingPage,
-      errorPage,
-      profile,
-      articleDeleted,
-    } = this.state;
-
-    const { slug } = this.props.match.params;
-    const { token, username } = this.props.authInfo;
-
-    if (articleDeleted) {
-      return <Redirect to={`/profile/${username}`} />;
-    }
-
-    const elements =
-      article.tagList &&
-      article.tagList.map((item, index) => {
-        return (
-          <li key={index} className="tag-default tag-pill tag-outline">
-            {item}
-          </li>
-        );
-      });
-
-    const yourArticle = (
-      <span>
-        <Link to={`/editor/${slug}`}>
-          <button className="btn btn-sm btn-outline-secondary">
-            <FontAwesomeIcon icon={faEdit} /> Edit Article
-          </button>
-        </Link>
-        <button
-          className="btn btn-sm btn-outline-danger"
-          onClick={() => this.deleteArticle(slug, token)}
-        >
-          <FontAwesomeIcon icon={faTrashAlt} />
-          Delete Article
-        </button>
-      </span>
-    );
-
-    const anotherArticle = (
-      <span className="article-buttons">
-        <FollowProfile
-          author={profile.username}
-          following={profile.following}
-        />
-        <FavouriteLike
-          className="btn btn-sm btn-outline-primary"
-          favoritesCount={article.favoritesCount}
-          favorited={article.favorited}
-          slug={article.slug}
-          page={true}
-        ></FavouriteLike>
-      </span>
-    );
-    const articleButtons =
-      article.author === username ? yourArticle : anotherArticle;
-
-    if (errorPage) {
-      return <ErrorComponent />;
+    
+    if(errorPage) {
+      return <Redirect to={"/"} />
     }
 
-    if (loadingPage) {
-      return <Spinner />;
-    }
+		const elements =
+			article.tagList &&
+			article.tagList.map((item, index) => {
+				return (
+					<li key={index} className="tag-default tag-pill tag-outline">
+						{item}
+					</li>
+				);
+			});
 
-    return (
-      <div className="article-page">
-        <div className="banner">
-          <div className="container">
-            <h1>{article.title}</h1>
-            <div>
-              <div className="article-meta">
-                <Link to={`/profile/${article.author}`}>
-                  <img src={article.image} alt="" />
-                </Link>
-                <div className="info">
-                  <Link to={`/profile/${article.author}`} className="author">
-                    {article.author}
-                  </Link>
-                  <span className="date">{article.updatedAt}</span>
-                </div>
-                {articleButtons}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="container article-content">
-          <div className="row">
-            <div className="col-md-12">
-              <div>
-                <p>{article.body}</p>
-              </div>
-              <ul className="tag-list">{elements}</ul>
-            </div>
-          </div>
-        </div>
-        <hr></hr>
-      </div>
-    );
-  }
+		const yourArticle = (
+			<span>
+				<Link to={`/editor/${slug}`}>
+					<button className="btn btn-sm btn-outline-secondary">
+						<FontAwesomeIcon icon={faEdit} /> Edit Article
+					</button>
+				</Link>
+				<button className="btn btn-sm btn-outline-danger" onClick={() => this.deleteArticle(slug, token)}>
+					<FontAwesomeIcon icon={faTrashAlt} />
+					Delete Article
+				</button>
+			</span>
+		);
+
+		const anotherArticle = (
+			<span className="article-buttons">
+				<FollowProfile author={profile.username} following={profile.following} />
+				<FavouriteLike
+					className="btn btn-sm btn-outline-primary"
+					favoritesCount={article.favoritesCount}
+					favorited={article.favorited}
+					slug={article.slug}
+					page={true}
+				></FavouriteLike>
+			</span>
+		);
+		const articleButtons = article.author === username ? yourArticle : anotherArticle;
+
+		if (loadingPage) {
+			return <Spinner />;
+		}
+
+		return (
+			<div className="article-page">
+				<div className="banner">
+					<div className="container">
+						<h1>{article.title}</h1>
+						<div>
+							<div className="article-meta">
+								<Link to={`/profile/${article.author}`}>
+									<img src={article.image} alt="" />
+								</Link>
+								<div className="info">
+									<Link to={`/profile/${article.author}`} className="author">
+										{article.author}
+									</Link>
+									<span className="date">{article.updatedAt}</span>
+								</div>
+								{articleButtons}
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="container article-content">
+					<div className="row">
+						<div className="col-md-12">
+							<div>
+								<p>{article.body}</p>
+							</div>
+							<ul className="tag-list">{elements}</ul>
+						</div>
+					</div>
+				</div>
+				<hr></hr>
+			</div>
+		);
+	}
 }
 
 const ArticleDetails = withUser(withRouter(ArticlePage));
--- src/components/profile-component/profile-page/profile-page.js
+++ src/components/profile-component/profile-page/profile-page.js
@@ -1,208 +1,198 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { withRouter } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEdit } from "@fortawesome/free-solid-svg-icons";
-import ProfileFeed from "../profile-feed";
-import ArticleList from "../../shared/article-list";
-import FollowProfile from "../follow-profile";
-import {
-  getAuthorArticles,
-  getFavouritedArticles,
-} from "../../../services/article-service";
-import { getProfile } from "../../../services/profile-service";
-import withUser from "../../../hocs";
-import Spinner from "../../shared/spinner";
-import ErrorComponent from "../../shared/error-component";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router';
+import { withRouter } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import ProfileFeed from '../profile-feed';
+import ArticleList from '../../shared/article-list';
+import FollowProfile from '../follow-profile';
+import { getAuthorArticles, getFavouritedArticles } from '../../../services/article-service';
+import { getProfile } from '../../../services/profile-service';
+import withUser from '../../../hocs';
+import Spinner from '../../shared/spinner';
 
-import "./profile-page.css";
+import './profile-page.css';
 
 class ProfilePage extends Component {
-  _isMounted = false;
+	_isMounted = false;
 
-  state = {
-    articles: [],
-    articlesCount: null,
-    indexPagination: 1,
-    limit: 10,
-    authorData: [],
-    loadingPage: true,
-    errorPage: false,
-    loadingArticles: true,
-    errorArticles: false,
-    typeFeed: "myPosts",
-  };
+	state = {
+		articles: [],
+		articlesCount: null,
+		indexPagination: 1,
+		limit: 10,
+		authorData: [],
+		loadingPage: true,
+		errorPage: false,
+		loadingArticles: true,
+		errorArticles: false,
+		typeFeed: 'myPosts',
+	};
 
-  componentDidMount() {
-    this._isMounted = true;
-    this.loadPage();
-    this.loadArticles();
-  }
+	componentDidMount() {
+		this._isMounted = true;
+		this.loadPage();
+		this.loadArticles();
+	}
 
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
+	componentWillUnmount() {
+		this._isMounted = false;
+	}
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.props.match.params.author !== prevProps.match.params.author) {
-      this.loadPage();
-    }
-    if (this.state.typeFeed !== prevState.typeFeed) {
-      this.loadArticles();
-    }
-  }
+	componentDidUpdate(prevProps, prevState) {
+		if (this.props.match.params.author !== prevProps.match.params.author) {
+			this.loadPage();
+		}
+		if (this.state.typeFeed !== prevState.typeFeed) {
+			this.loadArticles();
+		}
+	}
 
-  loadPage() {
-    const { author } = this.props.match.params;
-    const { token } = this.props.authInfo;
-    if (!author) {
-      return;
-    }
-    getProfile(author, token)
-      .then((profile) => {
-        if (this._isMounted) {
-          this.setState({
-            authorData: profile,
-            loadingPage: false,
-          });
-        }
-      })
-      .catch((e) => {
-        if (this._isMounted) {
-          this.setState({ errorPage: true });
-        }
-      });
-  }
+	loadPage() {
+		const { author } = this.props.match.params;
+		const { token } = this.props.authInfo;
+		if (!author) {
+			return;
+		}
+		getProfile(author, token)
+			.then((profile) => {
+				if (this._isMounted) {
+					this.setState({
+						authorData: profile,
+						loadingPage: false,
+					});
+				}
+			})
+			.catch((e) => {
+				if (this._isMounted) {
+					this.setState({ errorPage: true });
+				}
+			});
+	}
 
-  loadArticles = (offset = 0) => {
-    const payLoad = {
-      author: this.props.match.params.author,
-      limit: this.state.limit,
-      offset,
-      token: this.props.authInfo.token,
-    };
+	loadArticles = (offset = 0) => {
+		const payLoad = {
+			author: this.props.match.params.author,
+			limit: this.state.limit,
+			offset,
+			token: this.props.authInfo.token,
+		};
 
-    let serviceName;
-    if (this.state.typeFeed === "myPosts") {
-      serviceName = getAuthorArticles;
-    } else if (this.state.typeFeed === "favouritedPosts") {
-      serviceName = getFavouritedArticles;
-    }
+		let serviceName;
+		if (this.state.typeFeed === 'myPosts') {
+			serviceName = getAuthorArticles;
+		} else if (this.state.typeFeed === 'favouritedPosts') {
+			serviceName = getFavouritedArticles;
+		}
 
-    serviceName(payLoad)
-      .then((data) => {
-        if (this._isMounted) {
-          this.setState({
-            articles: data.articles,
-            articlesCount: data.articlesCount,
-            loadingArticles: false,
-          });
-        }
-      })
-      .catch((e) => {
-        if (this._isMounted) {
-          this.setState({ errorArticles: true });
-        }
-      });
-  };
+		serviceName(payLoad)
+			.then((data) => {
+				if (this._isMounted) {
+					this.setState({
+						articles: data.articles,
+						articlesCount: data.articlesCount,
+						loadingArticles: false,
+					});
+				}
+			})
+			.catch((e) => {
+				if (this._isMounted) {
+					this.setState({ errorArticles: true });
+				}
+			});
+	};
 
-  clickHandler = (type) => {
-    this.setState({
-      typeFeed: type,
-      loadingArticles: true,
-      errorArticles: false,
-      indexPagination: 1,
-    });
-  };
+	clickHandler = (type) => {
+		this.setState({
+			typeFeed: type,
+			loadingArticles: true,
+			errorArticles: false,
+			indexPagination: 1,
+		});
+	};
 
-  PaginationClick = (page) => {
-    page = page + 1;
-    const { limit } = this.state;
-    const offset = page * limit - limit;
-    this.setState({
-      indexPagination: page,
-      loadingArticles: true,
-    });
-    this.loadArticles(offset);
-  };
+	PaginationClick = (page) => {
+		page = page + 1;
+		const { limit } = this.state;
+		const offset = page * limit - limit;
+		this.setState({
+			indexPagination: page,
+			loadingArticles: true,
+		});
+		this.loadArticles(offset);
+	};
 
-  render() {
-    const {
-      articles,
-      articlesCount,
-      limit,
-      indexPagination,
-      typeFeed,
-      loadingArticles,
-      errorArticles,
-      loadingPage,
-      errorPage,
-      authorData,
-    } = this.state;
+	render() {
+		const {
+			articles,
+			articlesCount,
+			limit,
+			indexPagination,
+			typeFeed,
+			loadingArticles,
+			errorArticles,
+			loadingPage,
+			errorPage,
+			authorData,
+		} = this.state;
 
-    const { author } = this.props.match.params;
-    const { token, username } = this.props.authInfo;
+		const { author } = this.props.match.params;
+		const { token, username } = this.props.authInfo;
 
-    const yourProfile = (
-      <Link to="/settings" className="ion-gear-a">
-        <button className="btn btn-sm btn-outline-secondary action-btn">
-          <FontAwesomeIcon icon={faEdit} /> Edit Profile Settings
-        </button>
-      </Link>
-    );
+		const yourProfile = (
+			<Link to="/settings" className="ion-gear-a">
+				<button className="btn btn-sm btn-outline-secondary action-btn">
+					<FontAwesomeIcon icon={faEdit} /> Edit Profile Settings
+				</button>
+			</Link>
+		);
 
-    const anotherProfile = (
-      <FollowProfile
-        author={authorData.username}
-        following={authorData.following}
-        token={token}
-      />
-    );
+		const anotherProfile = (
+			<FollowProfile author={authorData.username} following={authorData.following} token={token} />
+		);
 
-    const profileButton =
-      author === username && !loadingPage && !errorPage
-        ? yourProfile
-        : anotherProfile;
+		const profileButton = author === username && !loadingPage && !errorPage ? yourProfile : anotherProfile;
 
-    if (errorPage) {
-      return <ErrorComponent />;
-    }
+		if (errorPage) {
+			return <Redirect to={'/'} />;
+		}
 
-    if (loadingPage) {
-      return <Spinner />;
-    }
-    return (
-      <>
-        <div className="profile-page">
-          <div className="user-info">
-            <div className="container">
-              <div className="row">
-                <div className="col-xs-12 col-md-10 offset-md-1">
-                  <img className="user-img" src={authorData.image} alt="" />
-                  <h4>{authorData.username}</h4>
-                  <p>{authorData.bio}</p>
-                  <div>{profileButton}</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="container">
-          <ProfileFeed typeFeed={typeFeed} onClickHandler={this.clickHandler} />
-          <ArticleList
-            articles={articles}
-            loading={loadingArticles}
-            error={errorArticles}
-            limit={limit}
-            articlesCount={articlesCount}
-            onPaginationClick={this.PaginationClick}
-            indexPagination={indexPagination}
-            token={token}
-          />
-        </div>
-      </>
-    );
-  }
+		if (loadingPage) {
+			return <Spinner />;
+		}
+		return (
+			<>
+				<div className="profile-page">
+					<div className="user-info">
+						<div className="container">
+							<div className="row">
+								<div className="col-xs-12 col-md-10 offset-md-1">
+									<img className="user-img" src={authorData.image} alt="" />
+									<h4>{authorData.username}</h4>
+									<p>{authorData.bio}</p>
+									<div>{profileButton}</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="container">
+					<ProfileFeed typeFeed={typeFeed} onClickHandler={this.clickHandler} />
+					<ArticleList
+						articles={articles}
+						loading={loadingArticles}
+						error={errorArticles}
+						limit={limit}
+						articlesCount={articlesCount}
+						onPaginationClick={this.PaginationClick}
+						indexPagination={indexPagination}
+						token={token}
+					/>
+				</div>
+			</>
+		);
+	}
 }
 
 const ProfileDetails = withUser(withRouter(ProfilePage));
